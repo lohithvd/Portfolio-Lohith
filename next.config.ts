af9920fc37f7b68aea514bb605d29f7bcfb0d0ad
PraveenGongada <praveengongada0603@gmail.com>
2025-04-19 02:09:14 +0530

    (fix) Custom Domain Linking

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,12 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  basePath: '',
+  assetPrefix: '',
+  output: 'export',
+  trailingSlash: true,
   images: {
+    unoptimized: true,
     remotePatterns: [
       {
         protocol: "https",
